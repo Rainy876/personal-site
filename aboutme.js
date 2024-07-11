71c6bc8403b82f0f098599c5f73e55a5f236e623
Logan Edwards <logansthebest99@gmail.com>
2024-07-11 09:19:10 -0400

    Add 01 proj files

--- aboutme.js
+++ aboutme.js
@@ -1,8 +1,9 @@
 const checkbox = document.getElementById("checkbox");
-
+var x = document.getElementById("toucan");
+x.style.display = "none";
 function checkboxChange() {
-    var x = document.getElementById("toucan");
-    if (x.style.display === "none") {
+    
+    if (x.style.display == "none") {
         x.style.display = "block";
       } else {
         x.style.display = "none";
